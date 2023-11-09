3ea4c9d61b2bce6db141c3ad0f1048edbfb17da9
Andreas Hinz <andreas.hinz@bechtle.com>
2023-11-09 11:43:35 +0100

    Added more explanation for the state machine. States and the possible transitions are declared in detail.

--- src/UserProfileService.Saga.StateMachine/Services/RolePropertiesChangedMessageService.cs
+++ src/UserProfileService.Saga.StateMachine/Services/RolePropertiesChangedMessageService.cs
@@ -8,15 +8,6 @@ using UserProfileService.Events.Implementation.V2;
 using UserProfileService.Events.Payloads.V2;
 using UserProfileService.Saga.Events.Messages;
 using UserProfileService.Saga.Validation.Abstractions;
-using Maverick.UserProfileService.AggregateEvents.Common;
-using Maverick.UserProfileService.Models.BasicModels;
-using Microsoft.Extensions.Logging;
-using UserProfileService.Commands;
-using UserProfileService.Common.Logging.Extensions;
-using UserProfileService.Events.Implementation.V2;
-using UserProfileService.Events.Payloads.V2;
-using UserProfileService.Saga.Events.Messages;
-using UserProfileService.Saga.Validation.Abstractions;
 using UserProfileService.StateMachine.Abstraction;
 using UserProfileService.StateMachine.Utilities;
 
