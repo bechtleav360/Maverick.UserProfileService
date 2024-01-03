17e4c135f587e1b9cc79ae8ba27f394988e3d6b3
Andreas Hinz <andreas.hinz@bechtle.com>
2024-01-03 09:48:18 +0100

    Fixed Arango configuration client serializer settings.

---
+++ tests/UserProfileService.Arango.IntegrationTests/V2/Serializer/FirstLevelProjectionTestArangoClientSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using JsonSubTypes;
+using Maverick.Client.ArangoDb.Public.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using UserProfileService.Adapter.Arango.V2.Helpers;
+using UserProfileService.Arango.IntegrationTests.V2.Implementations;
+using UserProfileService.Common.V2.TicketStore.Abstractions;
+
+namespace UserProfileService.Arango.IntegrationTests.V2.Serializer;
+
+public class FirstLevelProjectionTestArangoClientSettings: IArangoClientJsonSettings
+{
+    public JsonSerializerSettings SerializerSettings { get; set; } = new JsonSerializerSettings
+                                                                     {
+                                                                         Converters = WellKnownJsonConverters
+                                                                             .GetDefaultFirstLevelProjectionConverters()
+                                                                             .Append(  JsonSubtypesConverterBuilder
+                                                                                 .Of<TicketBase>(nameof(TicketBase.Type))
+                                                                                 .RegisterSubtype<TicketA>("TicketA")
+                                                                                 .RegisterSubtype<TicketB>("TicketB")
+                                                                                 .Build())
+                                                                             .Concat(WellKnownJsonConverters.GetDefaultProfileConverters())
+                                                                             .ToList(),
+                                                                         ContractResolver =
+                                                                             new DefaultContractResolver()
+                                                                     };
+}
