da3a5df1e418471bbd7d26db83044e2ee241f593
Andreas Hinz <andreas.hinz@bechtle.com>
2024-01-23 17:26:10 +0100

    Added additional files to that the sync is working.

---
+++ src/UserProfileService.Sync/Extensions/StringExtension.cs
@@ -0,0 +1,29 @@
+﻿
+using Maverick.UserProfileService.Models.EnumModels;
+using UserProfileService.Sync.Abstraction.Contracts;
+
+namespace UserProfileService.Sync.Extensions;
+
+/// <summary>
+///     Class that contains string extensions.
+/// </summary>
+public static class StringExtension
+{
+    /// <summary>
+    ///     Returns for the string the right object type.
+    /// </summary>
+    /// <param name="entity">The entity that should be returned as <see cref="ObjectType" />.</param>
+    /// <returns>The object type <see cref="ObjectType" />.</returns>
+    public static ObjectType GetObjectType(this string entity)
+    {
+        return entity switch
+               {
+                   SyncConstants.SagaStep.GroupStep => ObjectType.Group,
+                   SyncConstants.SagaStep.RoleStep => ObjectType.Role,
+                   SyncConstants.SagaStep.UserStep => ObjectType.User,
+                   SyncConstants.SagaStep.OrgUnitStep => ObjectType.Organization,
+                   SyncConstants.SagaStep.FunctionStep => ObjectType.Function,
+                   _ => ObjectType.Unknown
+               };
+    }
+}
