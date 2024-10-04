1a7ba6dab28d267859f508e19f77829f37e6e79a
Andreas Hinz <andreas.hinz@bechtle.com>
2024-10-04 16:15:36 +0200

    Removed lazy block and internal method.

--- src/UserProfileService/Utilities/UserContextStore.cs
+++ src/UserProfileService/Utilities/UserContextStore.cs
@@ -1,7 +1,6 @@
 ﻿using Maverick.UserProfileService.Models.Abstraction;
 using Maverick.UserProfileService.Models.BasicModels;
 using UserProfileService.Common.Logging.Extensions;
-using UserProfileService.Common.V2.Abstractions;
 using UserProfileService.Common.V2.Extensions;
 
 namespace UserProfileService.Utilities;
@@ -13,37 +12,27 @@ public class UserContextStore : IUserContextStore
 {
     private readonly HttpContext _context;
     private readonly ILogger<UserContextStore> _logger;
-    private readonly Lazy<string> _userId;
 
     /// <summary>
     ///     Initializes a new instance of <see cref="UserContextStore" />.
     /// </summary>
     /// <param name="logger">The<see cref="ILogger{T}" /> which should be used for logging.</param>
     /// <param name="contextAccessor">Used to retrieve the <see cref="HttpContext" /> to gather auth information from.</param>
-    /// <param name="readService">A <see cref="IReadService" /> which will be used in order to gather the ups-internal id.</param>
     public UserContextStore(
         ILogger<UserContextStore> logger,
-        IHttpContextAccessor contextAccessor,
-        IReadService readService)
+        IHttpContextAccessor contextAccessor)
     {
         _logger = logger;
         _context = contextAccessor.HttpContext;
-        _userId = new Lazy<string>(GetUserIdInternal);
     }
-
-    private string GetUserIdInternal()
-    {
-        _logger.EnterMethod();
-
-        string userId = _context.GetUserId(_logger);
-            
-        return _logger.ExitMethod<string>(userId);
-    }
-    
 
     /// <inheritdoc cref="IUserContextStore.GetIdOfCurrentUser" />
     public string GetIdOfCurrentUser()
     {
-        return _userId.Value;
+        _logger.EnterMethod();
+
+        string userId = _context.GetUserId(_logger);
+
+        return _logger.ExitMethod<string>(userId);
     }
 }
